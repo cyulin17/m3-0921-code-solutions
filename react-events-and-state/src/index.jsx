5c94a54d59c6acf5225a2b2c14432eded0f6f076
ChiaYu Lin <lin.1008@osu.edu>
2021-10-12 19:15:46 -0700

    Completed react-events-and-state.

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({ isClicked: true });
+  }
+
+  render() {
+    return (
+      <button onClick={ this.handleClick }> {this.state.isClicked ? 'Thanks!' : 'Click Me!'} </button>
+    );
+  }
+}
+
+const element = (
+  <div>
+    <CustomButton />
+  </div>
+);
+
+ReactDom.render(
+  element,
+  document.querySelector('#root')
+);
