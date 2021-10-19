3f3d86cf0b17208eda5ea1f903bb21794a8b4c43
ChiaYu Lin <lin.1008@osu.edu>
2021-10-18 20:35:41 -0700

    Completed react-form-controls.

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class NewsletterForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handleSubmit(event) {
+    console.log('state:', this.state);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        Email<input onChange={this.handleChange} type="text"></input><button>Sign Up</button>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NewsletterForm />,
+  document.querySelector('#root')
+);
